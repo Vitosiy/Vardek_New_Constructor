a26ea24812f9d01abc36b703d3b5d2ee14e69fe4
a9500484054 <a9500484054@yandex.ru>
2025-10-09 11:54:25 +0300

    Исправление ошибки запроса цены

--- src/views/The3D.vue
+++ src/views/The3D.vue
@@ -113,7 +113,7 @@ const CutSave = ref(false);
 // Список событий
 const priceUpdateEvents  = [
     // "A:Move",
-    "A:Selected",
+    // "A:Selected",
     // "A:ContantLoaded", 
     // "A:ClearSelected",
     // "A:ScreenPrint",
