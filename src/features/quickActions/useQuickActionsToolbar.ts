f53effb2bd114aa9fd3b5ce62c0112b7cba98259
andrew_359 <ndrew.359@gmail.com>
2025-12-11 18:27:14 +0300

    fixed new project button /w basket

--- src/features/quickActions/useQuickActionsToolbar.ts
+++ src/features/quickActions/useQuickActionsToolbar.ts
@@ -16,6 +16,7 @@ import { useRoomState } from '@/store/appliction/useRoomState';
 import { useSchemeTransition } from '@/store/canvasMerge/schemeTransition';
 import { loadBlankRoom } from '@/Constructor2D/facade/blankRoom';
 import { useBasketStore } from '@/store/appStore/useBasketStore';
+import { useRoomContantData } from '@/store/appliction/useRoomContantData';
 
 export type ActionKey =
   | 'fullscreen'
@@ -155,7 +156,10 @@ export const useQuickActionsToolbar = () => {
       tooltip: 'На весь экран',
       iconClass: 'icon-centered',
       path: 'default',
-      action: () => toggleFullscreen(),
+      action: () => {
+        toggleFullscreen()
+        console.log('SCHEME TRANSITION', schemeTransition.getSchemeTransitionData);
+      }
     },
     {
       key: 'drowmod',
@@ -258,25 +262,29 @@ export const useQuickActionsToolbar = () => {
         if (router.currentRoute.value.path !== '/2d') {
           await router.push('/2d');
         }
+        // Ждем, пока роутер завершит переход и компонент начнет монтироваться
+        await new Promise(resolve => setTimeout(resolve, 100));
+        
         schemeTransition.clearStore();
         sceneState.createNewProject();
         roomState.rooms = [];
         roomState.clearCurrentRoomId();
         projectState.resetState();
         useBasketStore().clearBasket()
+        useRoomContantData().setRoomContantDataForBasket({})
         try {
           const c2d = (window as any).C2D;
           if (c2d?.layers) {
-            c2d.layers.planner.clear();
-            c2d.layers.doorsAndWindows.clear();
-            c2d.layers.dimensionDisplay.clearAll();
-            c2d.layers.arrowRulerActiveObject.clearGraphic();
-            c2d.layers.startPointActiveObject.activate(false);
+            c2d.layers.planner?.clear();
+            c2d.layers.doorsAndWindows?.clear();
+            c2d.layers.dimensionDisplay?.clearAll();
+            c2d.layers.arrowRulerActiveObject?.clearGraphic();
+            c2d.layers.startPointActiveObject?.activate(false);
           }
         } catch (error) {
           console.warn('Ошибка при очистке слоев C2D:', error);
         }
-        await loadBlankRoom();
+        await loadBlankRoom()
       },
     },
   ];
